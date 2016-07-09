5778df283938a27cc4082e66b2db5c21a3026467
Nick Reid <nickreid@nickreid.com>
2016-07-09 14:18:43 -0700

    removed header navigation

--- src/common/motech-list/list.directive.js
+++ src/common/motech-list/list.directive.js
@@ -9,10 +9,6 @@
      * @ngdoc directive
      * @name  motechList
      *
-     * @param {Boolean} Loading Indicates if data that will be displayed is loading.
-     * @param {String} sortIndex Id of column that list is currently sorted by.
-     * @param {Boolean} sortDesc Indicates if the list is sorted in descending order.
-     *
      * @description 
      * Creates the frame around a list of objects which are rendered inside a transclude block. This directive exposes scope variables that can be used to set or get the state of the list.
      *
@@ -37,93 +33,8 @@
                 changeSort: '&'
             },
             controller: 'MotechListController',
-            controllerAs: 'MotechListCtrl',
-            link: dataGridDirective
+            controllerAs: 'MotechListCtrl'
         };
-    }
-
-    /**
-     * The link function for motech-list is responsible for rendering the column headings in the motech-list.
-     * 
-     * @memberOf motechList
-     *
-     * @param  {object} scope Scope of directive
-     * @param  {DOMElement} element DOM element of directive
-     * @param  {object} attrs Attributes from directive's DOM element
-     * @param  {ngController} ctrl Reference to MotechListController
-     *
-     */
-    function dataGridDirective(scope, element, attrs, ctrl){
-        var totalColumnSpace;
-        jQuery(window).resize(resizeGrid);
-        scope.$on('motech-list.ready', resizeGrid);
-        function resizeGrid(){
-            if(ctrl.hasCollapsibleColumn){
-                element.addClass('has-collapsible-content');
-            } else {
-                element.removeClass('has-collapsible-content');
-            }
-
-            element.width(element.parent().width());
-            ctrl.columns.forEach(setColumnWidth);
-            jQuery('.motech-list-rows li', element).each(function(){
-                var row = jQuery(this);
-                var totalWidth = row.width();
-                var columnsSpace = 0;
-                var hasMainItem = false;
-                row.children().each(function(){
-                    var cell = jQuery(this);
-                    if(cell.hasClass('list-item-main')){
-                        hasMainItem = true;
-                    }
-                    if(!cell.hasClass('list-item-main') && !cell.hasClass('motech-list-collapsible')){
-                        columnsSpace += cell.outerWidth();
-                    }
-                });
-                var extraSpace = totalWidth - columnsSpace; // 17px off?
-                if(hasMainItem){
-                    row.children('.list-item-main').width(extraSpace);
-                } else {
-                    
-                }
-            });
-
-            var widest = 0;
-            jQuery('.list-item-main', element).each(function(){
-                var cell = jQuery(this);
-                if(cell.outerWidth() > widest){
-                    widest = cell.outerWidth();
-                }
-            });
-            jQuery('.list-item-main', element).outerWidth(widest);
-
-            jQuery('.motech-list-column-headers', element).children().each(function(){
-                var col = jQuery(this);
-                var widest = 0;
-                jQuery('.motech-list-rows [motech-list-column-id="'+col.attr('motech-list-column-id')+'"]').each(function(){
-                    var cell = jQuery(this);
-                    if(cell.outerWidth() > widest){
-                        widest = cell.outerWidth();
-                    }
-                });
-                col.width(widest);
-            });
-        }
-
-        function setColumnWidth(col){
-            var widest = 0;
-            if(jQuery('.motech-list-rows [motech-list-column-id="'+col.id+'"]', element).parent().hasClass('list-item-main')){
-                return false;
-            }
-            jQuery('.motech-list-rows [motech-list-column-id="'+col.id+'"]', element).each(function(){
-                var cell = jQuery(this);
-                if(cell.outerWidth() > widest){
-                    widest = cell.outerWidth() + 1;
-                }
-            });
-            jQuery('.motech-list-rows [motech-list-column-id="'+col.id+'"]', element)
-            .width(widest);
-        }
     }
     
 })();
